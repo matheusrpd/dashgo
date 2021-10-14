395020d6921ee7bdfa212aaef1a51549a7d3db71
matheusrpd <matheusrpd@gmail.com>
2021-10-14 17:00:29 -0300

    Adicionando navegação na aplicação.

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { Box, Flex, Heading, Divider, VStack, SimpleGrid, HStack, Button } from "@chakra-ui/react";
 import { Input } from "../../components/Form/Input";
 import { Header } from "../../components/Header";
@@ -36,7 +37,9 @@ export default function UserCreate() {
 
           <Flex mt="8" justify="flex-end">
             <HStack spacing="4">
-              <Button colorScheme="whiteAlpha">Cancelar</Button>
+              <Link href="/users" passHref>
+                <Button as="a" colorScheme="whiteAlpha">Cancelar</Button>
+              </Link>
               <Button colorScheme="pink">Salvar</Button>
             </HStack>
           </Flex>
